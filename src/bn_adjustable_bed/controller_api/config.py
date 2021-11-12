92e009aa5ed1516fe1b35d8a2384a20ab9fed3da
Trevor Lauder <trevor@lauder.family>
2021-11-11 20:22:22 -0700

    Build docker image with package from pypi

--- src/bn_adjustable_bed/controller_api/config.py
+++ src/bn_adjustable_bed/controller_api/config.py
@@ -1,9 +1,13 @@
 # Copyright 2021 Trevor Lauder.
 # SPDX-License-Identifier: MIT
 
+import os
+
 import yaml
 
+config_path = f"{os.path.join(os.path.dirname(__file__))}/../config"
 
-def get_config(file: str = "config/controller_api.yml"):
+
+def get_config(file: str = f"{config_path}/controller_api.yml"):
     with open(file) as f:
         return yaml.safe_load(f.read())
